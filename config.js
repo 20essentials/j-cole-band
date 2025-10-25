e5dae59dffce97a8bd2feb9b368c7b402c1a0c62
DominicDev <s0l0estudi4r@gmail.com>
2025-10-25 12:34:06 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'J COLE ';
+export let nameSongs = [
+	" NO ROLE MODELZ (EXPLICIT)", /*1*/
+	"SHE KNOWS ", /*2*/
+	"APPARENTLY", /*3*/
+	"G.O.M.D.", /*4*/
+	"A TALE OF 2 CITIEZ", /*5*/
+	"03’ ADOLESCENCE", /*6*/
+	"JANUARY 28TH", /*7*/
+	"FIRE SQUAD", /*8*/
+	"NO ROLE MODELZ (ORIGINAL)", /*9*/
+	"WORK OUT", /*10*/
+	"CAN'T GET ENOUGH", /*11*/
+	"IN THE MORNING", /*12*/
+	"LOST ONES", /*13*/
+	"RISE AND SHINE", /*14*/
+	"DOLLAR AND A DREAM III", /*15*/
+	"BREAKDOWN", /*16*/
+	"WET DREAMZ", /*17*/
+	"SHE KNOWS (ORIGINAL)", /*18*/
+	"FORBIDDEN FRUIT", /*19*/
+	"LAND OF THE SNAKES", /*20*/
+]
